aabc9cc0fb73ecadbe65c6ab0957a29fbd9a26f7
Hannah Bingham <binghan.hb@gmail.com>
2019-09-24 10:09:47 -0500

    First user story searching for 'th' words complete with LINQ correctly returning 'th' containing strings.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,24 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+          List<string> words = new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var getWords = words.Where(w => w.Contains("th"));
+             foreach(var word in getWords)
+             {
+                 Console.WriteLine(word);
+             }
+            Console.ReadLine();
+         
+            
+            
         }
     }
+
+
+
+
+
+
 }
+    
+
