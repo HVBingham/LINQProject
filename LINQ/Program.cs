4e04fd775f616427a85a5d51b356239daa3c9b73
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-09-24 11:54:03 -0500

    Completed LINQ Problem 3. Removed lowest grade, then averages the rest of the grades form the string, then average the averages.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -25,6 +25,20 @@ namespace LINQ
                 Console.WriteLine(name);
             }
             Console.ReadLine();
+
+            List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+            var secondList = new List<double>();
+            double averageClassGrade = 0;
+            for(int i = 0; i < classGrades.Count; i++)
+            {
+                var splitNumbers = classGrades[i].Split(',').Select(double.Parse).ToList();
+                secondList = splitNumbers.OrderByDescending(c => c).Take(splitNumbers.Count() - 1).ToList();
+                var averageGrade = secondList.Average();
+                averageClassGrade += averageGrade;
+            }
+            double finalAverage = averageClassGrade / secondList.Count;
+            Console.WriteLine(finalAverage);
+            Console.ReadLine();
         }
     }
 }
