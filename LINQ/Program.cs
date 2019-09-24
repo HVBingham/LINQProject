57ddea4fd2db221e402ec4bbd634bf7640673fa0
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-09-24 10:31:00 -0500

    Completed LINQ Problem 2. Filtered strings to return a list of names without duplicates.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -17,6 +17,14 @@ namespace LINQ
                 Console.WriteLine(word);
             }
             Console.ReadLine();
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var getNames = names.Distinct();
+            foreach(var name in getNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
         }
     }
 }
