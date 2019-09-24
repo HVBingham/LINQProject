f9f436eb65926edd0361d697ca26c26d9b559cd7
Hannah Bingham <binghan.hb@gmail.com>
2019-09-24 11:51:40 -0500

    Third user story complete. Returns the correct average class grade when taking in each students average grades and removing the lowest grade from each students collection of grades.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,7 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            //1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             var getWords = words.Where(w => w.Contains("th"));
             foreach(var word in getWords)
@@ -17,7 +18,7 @@ namespace LINQ
                 Console.WriteLine(word);
             }
             Console.ReadLine();
-
+            //2
             List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
             var getNames = names.Distinct();
             foreach(var name in getNames)
@@ -25,6 +26,31 @@ namespace LINQ
                 Console.WriteLine(name);
             }
             Console.ReadLine();
+            //3
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+
+            };
+            var secondList = new List<double>();
+            double averageCGrade = 0;
+            double finalAverage = 0;
+            for(int i=0; i < classGrades.Count; i++)
+            {
+                var splitIt = classGrades[i].Split(',').Select(double.Parse).ToList();
+                secondList= splitIt.OrderByDescending(s => s).Take(splitIt.Count()-1).ToList();
+                var averageGrade = secondList.Average();
+                averageCGrade += averageGrade;  
+            }
+            finalAverage = averageCGrade / secondList.Count;
+            Console.WriteLine(finalAverage);
+            Console.ReadLine();
+            //4
+            
+          
         }
     }
 }
